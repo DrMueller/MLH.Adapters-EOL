37f63ab423b311843adb179dd3c8403b4b7ef6b9
Matthias Mueller <matthiasm@live.de>
2018-10-27 13:20:49 +0200

    Init

---
+++ Sources/Application.UnitTests/TestingAreas/Areas/Services/AdapterResolverUnitTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace Mmu.Mlh.Adapters.UnitTests.TestingAreas.Areas.Services
+{
+    [TestFixture]
+    public class AdapterResolverUnitTests
+    {
+    }
+}
---
+++ Sources/Application/Areas/Services/IAdapterResolver.cs
@@ -0,0 +1,9 @@
+﻿namespace Mmu.Mlh.ApplicationExtensions.Areas.Adapters.Services
+{
+    public interface IAdapterResolver
+    {
+        IAdapter<TDto, TModel> ResolveByAdapteeTypes<TDto, TModel>();
+
+        TAdapter ResolveByAdapterType<TAdapter>();
+    }
+}
